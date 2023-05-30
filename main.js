084463042f0bb1309685c26f4d2cdb32091f5fee
andrijevic-a <aleksandarandrijevic@gmail.com>
2023-05-30 12:49:18 +0200

    Added loading screen

--- main.js
+++ main.js
@@ -1,35 +1,40 @@
 // Smooth scrolling
 
-$('.navbar a, header .container div a').on('click', function(event) {
+$('.navbar a, header .container div a').on('click', function (event) {
   if (this.hash !== '') {
     event.preventDefault();
 
     const hash = this.hash;
 
     if (window.innerWidth > 768) {
-    $('html, body').animate (
-      {
-        scrollTop: $(hash).offset().top - 90
-      },
-      800
-    );} else {
-      $('html, body').animate (
+      $('html, body').animate(
         {
-          scrollTop: $(hash).offset().top - 50
+          scrollTop: $(hash).offset().top - 90,
+        },
+        800
+      );
+    } else {
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 50,
         },
         800
       );
     }
   }
+});
+
+// Video
+
+$(document).ready(function () {
+  $('.video-list video').click( () => {
+    $(this).addClass('active').siblings().removeClass('active');
+    let src = $(this).attr('src');
+    $('.main-video video').attr('src', src);
   });
+});
 
-
-  // Video
-
-    $(document).ready(function(){
-      $('.video-list video').click(function(){
-        $(this).addClass('active').siblings().removeClass('active');
-        let src = $(this).attr('src')
-        $('.main-video video').attr('src',src)
-      });
-    });
+window.addEventListener('load', () => {
+  const loadingScreen = document.getElementById('loading-screen');
+  loadingScreen.style.display = 'none';
+});
